d5c34ff659a743321fe87db6e7871857bd9e21ea
Xiaofang Jiang <xiaofang@xiaofangjiang.com>
2025-01-31 18:23:31 -0500

    try to make it host by githun.io

---
+++ build/precache-manifest.a76c605823e3f547bb40d0f087262385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91e977cebcbb2940959a0519305478e",
+    "url": "/identity_heatmap/index.html"
+  },
+  {
+    "revision": "f42e9a5c843bc4f2c183",
+    "url": "/identity_heatmap/static/css/2.8b960755.chunk.css"
+  },
+  {
+    "revision": "395bd696a73299f21a7a",
+    "url": "/identity_heatmap/static/css/main.ade32ced.chunk.css"
+  },
+  {
+    "revision": "f42e9a5c843bc4f2c183",
+    "url": "/identity_heatmap/static/js/2.6cd5df9d.chunk.js"
+  },
+  {
+    "revision": "395bd696a73299f21a7a",
+    "url": "/identity_heatmap/static/js/main.b78d1a52.chunk.js"
+  },
+  {
+    "revision": "8d50823f4aa2bafc5137",
+    "url": "/identity_heatmap/static/js/runtime~main.fd01ae48.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46b45844bf97b3f38508806aa44d3736.js"
+  "/identity_heatmap/precache-manifest.a76c605823e3f547bb40d0f087262385.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/identity_heatmap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
